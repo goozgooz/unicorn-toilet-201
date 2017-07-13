58540fe96754b46b3f500e36fed26187c05002e8
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-07-12 19:15:03 -0700

    starting work wednesday

--- app.js
+++ app.js
@@ -25,41 +25,50 @@ var porterPotty = new Toilet({lat: 47.620699, lng: -122.353726},'Sketch Porter P
 //array to hold all of our toilets
 var toilets = [sportsBar, armory, porterPotty];
 
-//initializing google map
-function initMap() {
-  var coords = {lat: 47.6164918, lng: -122.3511866};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: coords
-  });
-
-  //following lines of code creates a marker then gives that marker an info window and a event listener
-  function addMarker(obj){
-    var marker = new google.maps.Marker({
-      position: obj.coords,
-      map: map,
-      icon: obj.icon
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-      content: obj.info
-    });
-    marker.addListener('mouseover', function() {
-      infoWindow.open(map, marker);
-    });
-    marker.addListener('mouseout', function() {
-      infoWindow.close(map,marker);
-    });
-  }
-
-  //loops through our array with all the toilets and adds a marker and info window and event listener
-  for (var i = 0; i < toilets.length; i++) {
-    addMarker(toilets[i]);
-  }
-};
-
 //starting work on quiz
+//declaring all variables needed for DOM manipulation during the quiz
+var prompt = document.getElementById('prompt');
+var leftOption = document.getElementById('left-option');
+var rightOption = document.getElementById('right-option');
 
 function questionOne(){
+  prompt.innerHTML = 'Wait... How bad do you have to go to the bathroom??';
+  leftOption.setAttribute('src', 'images/gotta-go.jpg');
+  rightOption.setAttribute('src', 'images/hold-it.jpg');
 
 }
+
+questionOne();
+
+//initializing google map
+// function initMap() {
+//   var coords = {lat: 47.6164918, lng: -122.3511866};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 15,
+//     center: coords
+//   });
+//
+//   //following lines of code creates a marker then gives that marker an info window and a event listener
+//   function addMarker(obj){
+//     var marker = new google.maps.Marker({
+//       position: obj.coords,
+//       map: map,
+//       icon: obj.icon
+//     });
+//
+//     var infoWindow = new google.maps.InfoWindow({
+//       content: obj.info
+//     });
+//     marker.addListener('mouseover', function() {
+//       infoWindow.open(map, marker);
+//     });
+//     marker.addListener('mouseout', function() {
+//       infoWindow.close(map,marker);
+//     });
+//   }
+//
+//   //loops through our array with all the toilets and adds a marker and info window and event listener
+//   for (var i = 0; i < toilets.length; i++) {
+//     addMarker(toilets[i]);
+//   }
+// };
