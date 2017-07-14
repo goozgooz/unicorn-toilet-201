6e25991748d2c0e777fd028d588a7ffc0b0923fb
dlta <dlta.inc@icloud.com>
2017-07-13 20:13:42 -0700

    adding dlta's question

--- survey.js
+++ survey.js
@@ -13,18 +13,20 @@ function Toilet(coords, name, rating, tpQuality, drying, soap, parking, icon){
   this.icon = icon;       //default to ''. will change based on survey results
 };
 
-var sportsBar = new Toilet({lat: 47.6196, lng: -122.3487}, 'Sport Restaurant & Bar', 3, 2, 'Both', 'Gel', 'Pay');
+var sportsBar = new Toilet({lat: 47.6196, lng: -122.3487}, 'Sport Restaurant & Bar (Access Code-2587)', 3, 2, 'Both', 'Gel', 'Pay');
 var armory = new Toilet({lat: 47.621551, lng: -122.350939}, 'Armory @ Seattle Center', 4, 2, 'Paper Towel', 'Gel', 'Pay');
 var porterPotty = new Toilet({lat: 47.620699, lng: -122.353726},'Sketch Porter Potty', 1, 1, 'You Wish!', 'LOL', 'Free');
 var plazaGaribaldi = new Toilet({lat: 47.6194, lng: -122.3556}, 'Plaza Garibaldi', 3, 2, 'Paper Towel', 'Gel', 'Pay');
 var dominos = new Toilet({lat: 47.6178, lng: -122.3535}, 'Dominos', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A');
 var codeFellows = new Toilet({lat: 47.6182, lng: -122.3519}, 'Code Fellows', 4, 3, 'Paper Towel', 'Foam', 'Pay', 'images/code_fellows.png');
 var subway = new Toilet({lat: 47.618535, lng: -122.355325}, 'Subway', 3, 2, 'Paper Towel', 'Foam', 'Pay');
-var desertTan = new Toilet({lat:47.6175708,lng:-122.3533824},'Desert Tan', 3, 4,'Both','Foam','Pay');
-var cherryCafe = new Toilet({lat:47.6181642,lng:-122.3493946},'Cherry Street Coffee House', 3, 2, 'Paper Towel','Liquid','Pay');
+var desertTan = new Toilet({lat: 47.6175708,lng: -122.3533824},'Desert Tan', 3, 4,'Both','Foam','Pay');
+var cherryCafe = new Toilet({lat: 47.6181642,lng: -122.3493946},'Cherry Street Coffee House', 3, 2, 'Paper Towel','Liquid','Pay');
+var spaceNeedle = new Toilet({lat: 47.6202, lng: -122.3490}, 'Space Needle', 3, 3, 'Air Dryer', 'Gel', 'Pay');
+var komoPlaza = new Toilet({lat: 47.6194, lng: -122.3483}, 'Komo 4 Plaza (Access Code-3214)', 4, 1, 'Both', 'Gel', 'Pay');
 
 //array to hold all of our toilets
-var toilets = [sportsBar, armory, porterPotty, plazaGaribaldi, dominos, codeFellows, subway, desertTan, cherryCafe];
+var toilets = [sportsBar, armory, porterPotty, plazaGaribaldi, dominos, codeFellows, subway, desertTan, cherryCafe, spaceNeedle, komoPlaza];
 
 //starting work on quiz
 //declaring all variables needed for DOM manipulation during the quiz
@@ -66,7 +68,13 @@ function questionTwo(){
   leftOption.setAttribute('src', 'images/gel.jpg');
   rightOption.setAttribute('src', 'images/foam.jpg');
   var answer = event.target.id;
+}
 
+function questionThree(){
+  prompt.innerHTML = 'Which kind of bathroom selfie person are you?';
+  leftOption.setAttribute('src', 'images/selfie3.jpg');
+  rightOption.setAttribute('src', 'images/selfie4.jpg');
+  var answer = event.target.id;
 }
 
 //running the first queturnstion before things get going
@@ -95,6 +103,7 @@ function startQuiz(event){
   } if (turn < 5){
     event.preventDefault();
     questionTwo();
+    questionThree();
     // var answer = event.target.id;   //getting id of whichever option was picked in question1
   } else {
     createLocalStorage();
