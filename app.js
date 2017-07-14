dea112cc3fe7979471ea82866c7437421293381c
dlta <dlta.inc@icloud.com>
2017-07-13 20:38:48 -0700

    merging photo adds

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 
 'use strict';
 
-var toilets = [];
-// //object constructor
-// function Toilet(coords, name, rating, tpQuality, drying, soap, parking, icon){
-//   this.coords = coords;   // {lat:    , lng:    }
-//   this.name = name;
-//   this.rating = rating;   //Overall Rating | Integer 1-5
-//   this.tpQuality = tpQuality; //TP Quality Rating | Integer 1-5
-//   this.drying = drying;  //'Paper Towel' or 'Air Dryer' or 'Both'
-//   this.soap = soap;   // 'Gel' or 'Foam' Soap
-//   this.parking = parking; // 'Free' or 'Pay';
-//   this.info = '<h5>' + this.name + '</h5> <h6>Overall Rating: ' + this.rating + '</h6> <h6>TP Rating: ' + this.tpQuality + '</h6><h6>Soap: ' + this.soap + '</h6>' + '<h6>Drying Method: ' + this.drying + '</h6>' + '<h6>Parking: ' + this.parking + '</h6>';
-//   this.icon = icon;       //default to ''. will change based on survey results
-// };
-//
-// var sportsBar = new Toilet({lat: 47.6196, lng: -122.3487}, 'Sport Restaurant & Bar', 3, 2, 'Both', 'Gel', 'Pay');
-// var armory = new Toilet({lat: 47.621551, lng: -122.350939}, 'Armory @ Seattle Center', 4, 2, 'Paper Towel', 'Gel', 'Pay');
-// var porterPotty = new Toilet({lat: 47.620699, lng: -122.353726},'Sketch Porter Potty', 1, 1, 'You Wish!', 'LOL', 'Free');
-// var plazaGaribaldi = new Toilet({lat: 47.6194, lng: -122.3556}, 'Plaza Garibaldi', 3, 2, 'Paper Towel', 'Gel', 'Pay');
-// var dominos = new Toilet({lat: 47.6178, lng: -122.3535}, 'Dominos', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A');
-// var codeFellows = new Toilet({lat: 47.6182, lng: -122.3519}, 'Code Fellows', 4, 3, 'Paper Towel', 'Foam', 'Pay', 'images/code_fellows.png');
-// var subway = new Toilet({lat: 47.618535, lng: -122.355325}, 'Subway', 3, 2, 'Paper Towel', 'Foam', 'Pay');
-// var desertTan = new Toilet({lat:47.6175708,lng:-122.3533824},'Desert Tan', 3, 4,'Both','Foam','Pay');
-// var cherryCafe = new Toilet({lat:47.6181642,lng:-122.3493946},'Cherry Street Coffee House', 3, 2, 'Paper Towel','Liquid','Pay');
-//
-// //array to hold all of our toilets
-// var toilets = [sportsBar, armory, porterPotty, plazaGaribaldi, dominos, codeFellows, subway, desertTan, cherryCafe];
-var toiletData = JSON.parse(localStorage.data);
-var toilets = toiletData;
+//object constructor
+function Toilet(coords, name, rating, tpQuality, drying, soap, parking, icon){
+  this.coords = coords;   // {lat:    , lng:    }
+  this.name = name;  //Restaurant name
+  this.rating = rating;   //Overall Rating | Integer 1-5
+  this.tpQuality = tpQuality; //TP Quality Rating | Integer 1-5
+  this.drying = drying;  //'Paper Towel' or 'Air Dryer' or 'Both'
+  this.soap = soap;   // 'Gel' or 'Foam' Soap
+  this.parking = parking; // 'Free' or 'Pay';
+  this.info = '<h5>' + this.name + '</h5> <h6>Overall Rating: ' + this.rating + '</h6> <h6>TP Rating: ' + this.tpQuality + '</h6><h6>Soap: ' + this.soap + '</h6>' + '<h6>Drying Method: ' + this.drying + '</h6>' + '<h6>Parking: ' + this.parking + '</h6>';
+  this.icon = icon;       //default to ''. will change based on survey results
+};
+
+//creating toilet objects
+var sportsBar = new Toilet({lat: 47.6196, lng: -122.3487}, 'Sport Restaurant & Bar', 3, 2, 'Both', 'Gel', 'Pay');
+var armory = new Toilet({lat: 47.621551, lng: -122.350939}, 'Armory @ Seattle Center', 4, 2, 'Paper Towel', 'Gel', 'Pay');
+var porterPotty = new Toilet({lat: 47.620699, lng: -122.353726},'Sketch Porter Potty', 1, 1, 'You Wish!', 'LOL', 'Free');
+var costco = new Toilet({lat: 47.5654, lng: -122.3303}, 'Costco Wholesale', 4, 2, 'Both', 'Gel', 'Free');
+var italian = new Toilet({lat:47.6134, lng:122.3403}, 'Assaggio Restaurant', 5, 3, 'Paper Towel', 'Gel', 'Free');
+
+
+//array to hold all of our toilets
+var toilets = [sportsBar, armory, porterPotty];
 
 //initializing google map
 function initMap() {
